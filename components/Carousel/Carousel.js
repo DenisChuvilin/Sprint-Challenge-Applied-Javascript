944f885d5fa2859ae2ca094d7daa6104565609eb
DenisChuvilin <denischuv@gmail.com>
2020-03-13 10:00:01 -0700

    begun carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,12 @@
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to write a function that creates the carousel component, you will find the HTML below.
+
     2. You will need to grab a reference to all of the images
+
     3. Create a current index
+
     4. Those buttons are gonna need some click handlers.
+
     5. Think of how you would animate this component. Make the cards slide in and out, or fade. It's up to you!
     6. Have fun!
 */
@@ -17,3 +21,35 @@
     <div class="right-button"> > </div>
   </div>
 */
+// import images
+
+
+function carouselCreator(){
+  //create elements
+  const carousel = document.createElement('div');
+  const leftButton = document.createElement('div');
+  const img1 = document.createElement('img');
+  const img2 = document.createElement('img');
+  const img3 = document.createElement('img');
+  const img4 = document.createElement('img');
+  const rightButton = document.createElement('div');
+
+  // add classes 
+  carousel.classList.add('carousel');
+  leftButton.classList.add('left-button');
+  rightButton.classList.add('right-button');
+
+  // add attributes
+  img1.src = "../../assets/carousel/mountains.jpeg";
+  img2.src = "../../assets/carousel/computer.jpeg"
+  img3.src = "../../assets/carousel/trees.jpeg";
+  img4.src = "../../assets/carousel/turntable.jpeg";
+
+  //append elements
+  carousel.append(leftButton, img1, img2, img3, img4, rightButton);
+
+  return carousel;
+}
+
+const carouselContainer = document.querySelector('.carousel-container')
+carouselContainer.append(carouselCreator());
